d142849f2ab02b0bcf00a1faefd2d447d4d8a8f4
Marc Shen <marcshen@163.com>
2025-01-31 18:56:42 +0800

    update of the blog

--- src/consts.ts
+++ src/consts.ts
@@ -11,7 +11,7 @@ export const DATE_FORMAT = "ddd MMM DD YYYY";
 // User profile information
 export const USER_NAME = "Kadath Drawer";
 export const USER_SITE = "https://blog.stic.work"; // At the same time, this is also the site retrieved by the i18n configuration.
-export const USER_AVATAR = "/profile.jpeg";
+export const USER_AVATAR = "/profile.webp";
 
 // Server and transition settings
 export const SERVER_URL = "https://blog.stic.work";
